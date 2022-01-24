d2d74b925e88a783d081ce297a6d7a073c669472
cipher <ciphers@qq.com>
2022-01-24 17:11:12 +0800

    update craco config

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-const resolve = (dir: string) => path.resolve(__dirname, dir);
+const resolve = (dir) => path.resolve(__dirname, dir);
 
 const cracoConfig = {
   webpack: {
@@ -14,4 +14,4 @@ const cracoConfig = {
   }
 };
 
-export default cracoConfig;
+module.exports = cracoConfig;
